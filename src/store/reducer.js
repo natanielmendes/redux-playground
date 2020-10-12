71c5afb667764ef09a8f829a531ba9638a08eb1d
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-10-12 00:07:31 -0300

    Assignment 4: Time to Practice - Dispatching Actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,6 +8,21 @@ const reducer = (state = initialState, action) => {
             counter: state.counter + 1
         }
     }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === 'ADD_5') {
+        return {
+            counter: state.counter + 5
+        }
+    }
+    if (action.type === 'SUBTRACT_5') {
+        return {
+            counter: state.counter - 5
+        }
+    }
     return state;
 }
 
